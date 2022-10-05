0fab2151ac40f44b70f6b9606debc2a1708a60a8
akash <akash329@protonmail.com>
2022-10-05 19:18:26 +0530

    added FE and BE code

---
+++ user-mgmt-api/src/main/java/com/projects/usermgmt/UserMgmtApplication.java
@@ -0,0 +1,14 @@
+package com.projects.usermgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMgmtApplication.class, args);
+		System.out.println("Seems to be working....");
+	}
+
+}
---
+++ user-mgmt-api/src/main/java/com/projects/usermgmt/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.projects.usermgmt.repository;
+
+import com.projects.usermgmt.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+}
